f4516699a7fa175a056f6629a3c64d6aa9ef832b
is-ali25 <jabril_muhammad@brown.edu>
2025-01-12 19:26:39 -0500

    fixed a bug where edge seemed to go off to infinity when a node was modified in the GUI. The cedsk engine was changing the position mode of the modified node to 'absolute' rather than 'percent' which multiplied its numerical position value by 100. Solved this by just skeeping all shapes in absolute position mode

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ if (
     engine.block.setStrokeEnabled(block, true);
     engine.block.setStrokeColor(block, color2);
     engine.block.appendChild(page, block);
-    engine.block.setPositionX(block, 0.25);
-    engine.block.setPositionXMode(block, "Percent");
-    engine.block.setPositionY(block, 0.25);
-    engine.block.setPositionYMode(block, "Percent");
+    engine.block.setPositionX(block, 250);
+    // engine.block.setPositionXMode(block, "Percent");
+    engine.block.setPositionY(block, 250);
+    // engine.block.setPositionYMode(block, "Percent");
 
     let blob = engine.block.create("graphic");
     engine.block.setShape(blob, engine.block.createShape("ellipse"));
@@ -54,10 +54,10 @@ if (
     engine.block.setStrokeColor(blob, color2);
 
     engine.block.appendChild(page, blob);
-    engine.block.setPositionX(blob, 0.7);
-    engine.block.setPositionXMode(blob, "Percent");
-    engine.block.setPositionY(blob, 0.55);
-    engine.block.setPositionYMode(blob, "Percent");
+    engine.block.setPositionX(blob, 700);
+    // engine.block.setPositionXMode(blob, "Percent");
+    engine.block.setPositionY(blob, 550);
+    // engine.block.setPositionYMode(blob, "Percent");
 
     let line = engine.block.create("graphic");
     let lineFill = engine.block.createFill("color");
@@ -72,34 +72,23 @@ if (
     //edge between nodes
     setInterval(() => {
       const x1 =
-        engine.block.getPositionX(block) +
-        engine.block.getWidth(block) / (2 * pageWidth);
+        engine.block.getPositionX(block) + engine.block.getWidth(block) / 2;
       const y1 =
-        engine.block.getPositionY(block) +
-        engine.block.getHeight(block) / (2 * pageHeight);
+        engine.block.getPositionY(block) + engine.block.getHeight(block) / 2;
       const x2 =
-        engine.block.getPositionX(blob) +
-        engine.block.getWidth(blob) / (2 * pageWidth);
+        engine.block.getPositionX(blob) + engine.block.getWidth(blob) / 2;
       const y2 =
-        engine.block.getPositionY(blob) +
-        engine.block.getHeight(blob) / (2 * pageHeight);
+        engine.block.getPositionY(blob) + engine.block.getHeight(blob) / 2;
 
-      engine.block.setWidth(line, distance(x1, y1, x2, y2) * pageWidth);
+      engine.block.setWidth(line, distance(x1, y1, x2, y2));
       engine.block.setPositionX(line, x1);
-      engine.block.setPositionXMode(line, "Percent");
+      // engine.block.setPositionXMode(line, "Percent");
       engine.block.setPositionY(line, y1);
-      engine.block.setPositionYMode(line, "Percent");
+      // engine.block.setPositionYMode(line, "Percent");
       let angle = Math.acos(cos(x2 - x1, y2 - y1, 1, 0)); //angle b/w (1,0) and vector pointing from point1 to point2
       engine.block.setRotation(line, angle);
       engine.block.sendToBack(line);
     }, 30);
-
-    let toggle = 0.01;
-    setInterval(() => {
-      let xCoord = 0.25 + toggle;
-      engine.block.setPositionX(block, xCoord);
-      toggle = (toggle + 0.01) % 0.5;
-    }, 100);
 
     //append scene to HTML
     document.getElementById("cesdk_container").append(engine.element);
@@ -114,15 +103,15 @@ function distance(x1, y1, x2, y2) {
 
 function dot(x1, y1, x2, y2) {
   const dot = x1 * x2 + y1 * y2;
-  console.log(dot);
+  // console.log(dot);
   return dot;
 }
 
 function cos(x1, y1, x2, y2) {
   const mag1 = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
   const mag2 = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
-  console.log(mag1, mag2);
+  // console.log(mag1, mag2);
   const cos = dot(x1, y1, x2, y2) / (mag1 * mag2);
-  console.log(cos);
+  // console.log(cos);
   return cos;
 }
